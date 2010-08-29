7dd4534e424eab933ba0a04e666ff188648049dd
sungo <sungo@sungo.us>
2010-08-28 21:47:34 -0400

    dist::zilla-ification

--- lib/POE/API/Peek.pm
+++ lib/POE/API/Peek.pm
@@ -1,8 +1,5 @@
 package POE::API::Peek;
-
-=head1 NAME
-
-POE::API::Peek - Peek into the internals of a running POE environment
+# ABSTRACT: Peek into the internals of a running POE environment
 
 =head1 DESCRIPTION
 
@@ -27,8 +24,6 @@ B<Further, this module requires perl v5.6.1 or above.>
 use 5.006001;
 use warnings;
 use strict;
-
-our $VERSION = '2.16';
 
 
 BEGIN {
@@ -982,56 +977,5 @@ sub is_signal_watched_by_session {
 
 1;
 __END__
-
-=pod
-
-=head1 AUTHORS
-
-=over 4
-
-=item Matt Cashner (sungo@pobox.com)
-
-=item Yuval Kogman (nothingmuch@woobling.org)
-
-=back
-
-
-=head1 LICENSE
-
-=over 4
-
-=item Copyright (c) 2003 - 2008, 2010, Matt Cashner. 
-
-=item Copyright (c) 2008, Yuval Kogman
-
-=back
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-=over 4
-
-=item * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.  
-
-=item * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-=item * Neither the name of Matt Cashner nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-=back
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-=cut
 
 # sungo // vim: ts=4 sw=4 noet
